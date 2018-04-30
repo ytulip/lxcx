540798aaf66abda266ded84b4e4bdaa80e0ef76d
zhuyan <yi@zhuyan.me>
2018-04-30 10:59:05 +0800

    提交一下代码！

---
+++ pages/report/pay.js
@@ -0,0 +1,71 @@
+//index.js
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        openid:'',
+    },
+
+
+    onLoad:function(options)
+    {
+        var openid = util.auth.getOpenid();
+        this.setData(
+            {
+                openid:openid
+            }
+        );
+    },
+
+
+    bindPhoneInput: function(e){
+        this.setData({
+            immediatePhone: e.detail.value
+        })
+    },
+
+    pay:function () {
+        var requestData = {};
+        requestData.phone = this.data.immediatePhone;
+        wx.request({
+            url: util.serverHost + 'activity/report-pay?openid=' + this.data.openid,
+            method:'get',
+            data:requestData,
+            success:function(res)
+            {
+                console.log(res);
+                if( res.data.status)
+                {
+                    var jsonData = JSON.parse(res.data.data);
+                    console.log(jsonData);
+
+                    wx.requestPayment({
+                        'timeStamp': jsonData.timeStamp,
+                        'nonceStr': jsonData.nonceStr,
+                        'package': jsonData.package,
+                        'signType': jsonData.signType,
+                        'paySign': jsonData.paySign,
+                        'success':function(res){
+                            util.mAlert('支付成功');
+                            wx.redirectTo(
+                                {
+                                    url:'/pages/report/success'
+                                }
+                            );
+                        },
+                        'fail':function(res){
+                            util.mAlert('支付失败，请重新支付');
+                        }
+                    });
+                } else
+                {
+                    wx.showToast({
+                        title: res.data.desc,
+                        icon: "none",
+                        duration: 3000
+                    });
+                }
+            }
+        })
+    },
+})
