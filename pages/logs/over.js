9b737bad48face36e783d9fd8ffc6238b2cd32e6
zhuyan <yi@zhuyan.me>
2018-05-23 09:25:42 +0800

    成绩单！

--- pages/logs/over.js
+++ pages/logs/over.js
@@ -5,73 +5,37 @@ const app = getApp()
 
 Page({
     data: {
-        items1: [
-            {name: '1', value: '1、不吃早餐'},
-            {name: '2', value: '2、饭后松裤带'},
-            {name: '3', value: '3、饭后即睡'},
-            {name: '4', value: '4、饱食'},
-            {name: '5', value: '5、空腹吃糖'},
-            {name: '6', value: '6、吃太咸的食物'},
-        ],
-        items2: [
-            {name: '1', value: '1、起床先叠被'},
-            {name: '2', value: '2、伏案午睡'},
-            {name: '3', value: '3、俯睡'},
-            {name: '4', value: '4、睡前不洗脸'},
-            {name: '5', value: '5、睡前不刷牙'},
-            {name: '6', value: '6、睡懒觉'},
-        ],
-        items3: [
-            {name: '1', value: '1、男士留胡子'},
-            {name: '2', value: '2、跷二郎腿'},
-            {name: '3', value: '3、眯眼看东西、揉擦眼睛'},
-            {name: '4', value: '4、热水沐浴时间过长'},
-            {name: '5', value: '5、赌博'}
-        ],
-        items4: [
-            {name: '1', value: '1、生活过度紧张'},
-            {name: '2', value: '2、感冒了害怕洗澡'},
-            {name: '3', value: '3、觉得吃油腻食物会发胖'},
-            {name: '4', value: '4、认为刮掉腋毛，就能减少流汗'},
-            {name: '5', value: '5、流鼻血时要抬高下巴'},
-            {name: '6', value: '6、为了牙齿不蛀应该多刷牙'},
-            {name: '7', value: '7、饭后立即刷牙'},
-            {name: '8', value: '8、强忍小便'},
-        ],
-        itemsArray1:[],
-        itemsArray2:[],
-        itemsArray3:[],
-        itemsArray4:[],
-        image:""
+
     },
 
     onLoad:function()
     {
         var that = this;
-        wx.login({
-            success: function(res) {
-                if (res.code) {
-                    //发起网络请求
-                    wx.request({
-                        url: util.serverHost + 'activity/pool-info',
-                        data: {
-                            code: res.code
-                        },
-                        success:function(requestRes)
-                        {
-                            console.log(requestRes);
-                            // requestRes.data
-                            that.setData(
-                                {
-                                    openid:requestRes.data.data.openid,
-                                    image:requestRes.data.data.image
-                                }
-                            );
-                        }
-                    })
-                } else {
-                    console.log('登录失败！' + res.errMsg)
-                }
+        var openid = util.auth.getOpenid();
+        this.setData(
+            {
+                imageHost:util.imageHost
+            }
+        );
+        wx.request({
+            url: util.serverHost + 'activity/sign-report',
+            data: {
+                openid: openid,
+
+            },
+            success:function(requestRes)
+            {
+                console.log(requestRes);
+                // requestRes.data
+                that.setData(
+                    {
+                        healthInfo:JSON.parse(requestRes.data.data.user.health_info),
+                        openid:requestRes.data.data.openid,
+                        signRecord:JSON.parse(requestRes.data.data.sign_record.sign_prov),
+                        userInfo:requestRes.data.data.user,
+                        signDays:requestRes.data.data.sign_days
+                    }
+                );
             }
         });
     },
---
+++ pages/logs/turnback.js
@@ -0,0 +1,100 @@
+//index.js
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        openid:'',
+        product:{},
+        quantity:3,
+        price:0,
+        user:{},
+        countArray:['中国工商银行股份有限公司','中国农业银行股份有限公司','中国银行股份有限公司','中国建设银行股份有限公司','交通银行股份有限公司','中信银行股份有限公司','中国光大银行股份有限公司','华夏银行股份有限公司','中国民生银行股份有限公司','广发银行股份有限公司','平安银行股份有限公司','招商银行股份有限公司','兴业银行股份有限公司','中国邮政储蓄银行股份有限公司'],
+        countIndex:0,
+        account:'',
+        cash:0
+    },
+
+
+    onLoad:function(options)
+    {
+        var openid = util.auth.getOpenid();
+        this.setData(
+            {
+                openid:openid
+            }
+        );
+        var that = this;
+        wx.request({
+            url: util.serverHost + 'activity/user-info-new' ,
+            data: {
+                openid:openid
+            },
+            success:function(requestRes)
+            {
+                console.log(requestRes);
+                // requestRes.data
+
+                that.setData(
+                    {
+                        user:requestRes.data.data.user
+                    }
+                );
+            }
+        })
+    },
+
+    bindQuantityChange:function(e)
+    {
+        this.setData(
+            {
+                quantity:e.detail.value,
+                price:this.data.product.rebuy_price * e.detail.value
+            }
+        );
+    },
+
+    pay:function () {
+        var account = this.data.account;
+        if( !account )
+        {
+            util.mAlert('账号不能为空');
+            return;
+        }
+
+        var cash = this.data.cash;
+        if( cash <= 0 || cash > this.data.user.charge)
+        {
+            util.mAlert('请输入正确的提现金额');
+            return;
+        }
+
+        var type = this.data.countArray[this.data.countIndex];
+
+        wx.navigateTo(
+            {
+                url:'/pages/mine/withdrawconfirm?cash=' + cash + '&account=' + account + '&type=' + type
+            }
+        );
+    },
+
+    bindCountChange:function(e)
+    {
+        this.setData(
+            {countIndex:e.detail.value}
+        )
+    },
+
+    bindAccountInput:function(e)
+    {
+        this.setData(
+            {account:e.detail.value}
+        )
+    },
+
+    bindCashInput:function(e)
+    {
+        this.setData(
+            {cash:e.detail.value}
+        )
+    }
+})
